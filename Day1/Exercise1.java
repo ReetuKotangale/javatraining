570ab0d5b1ab1cf6f1771ebaeb383f2785a35084
ReetuKotangale <reetukotangale24@gmail.com>
2022-03-22 17:43:29 +0530

    test

---
+++ Day1/Exercise1.java
@@ -0,0 +1,10 @@
+public class Exercise1 {
+    public static void main(String args[]){
+        //calculate number of minutes in a day
+        int a=24*60;
+        System.out.println("Number of minutes in a day:"+a);
+        //calculate number of seconds in a day
+        int b=24*60*60;
+        System.out.println("Number of seconds in a day: "+b);
+    }
+}
