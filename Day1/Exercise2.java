c91b6a4910bca86ad7799feb0724dc8159c0d4c9
ReetuKotangale <reetukotangale24@gmail.com>
2022-03-22 18:18:38 +0530

    Exercise2 for day1

---
+++ Day1/Exercise2.java
@@ -0,0 +1,19 @@
+public class Exercise2 {
+    public static void main(String args[]){
+        //print Hello World
+        System.out.println("Hello world");
+        //print 5*3
+        System.out.println("5*3");
+        //calculate 5*3
+        System.out.printf("%d * %d = %d",5,3,5*3).println();
+        System.out.println(" ");
+        //print table for 5
+        for(int i=1; i<=10; i++){
+            System.out.printf("%d * %d = %d",5,i,5*i).println();
+        }
+        //print number of seconds in a day using System.out.println
+        int sec=24*60*60;
+        System.out.println("Number of seconds in a day: "+sec);
+
+    }
+}
